79fbd86086d8402eddc28449af22700c1950265e
Mike Salter <mikey.l.salter@gmail.com>
2022-07-04 12:14:30 +0100

    delete function added Users can now delete their own posts and unauthorized users are redirected to the main page

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -131,4 +131,24 @@ class Posts extends Controller
 
         $this->view('posts/show', $data);
     }
+
+    public function delete($id){
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            $post = $this->postModel->getPostById($id);
+            //Check ownership
+            if($post->user_id != $_SESSION['user_id']){
+                redirect('posts');
+            }
+
+            if($this->postModel->deletePost($id)){
+                flash('post_message', 'Post removed successfully');
+                redirect('posts');
+            } else {
+                die('Something has gone wrong');
+            }
+        } else {
+            redirect('posts');
+        }
+    }
 }
